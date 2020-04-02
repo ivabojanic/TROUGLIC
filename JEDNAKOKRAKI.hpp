1a2e70640276051067129be816a8c54897ae1a19
ivabojanic <62704225+ivabojanic@users.noreply.github.com>
2020-04-02 21:04:54 +0200

    Create JEDNAKOKRAKI.hpp

---
+++ JEDNAKOKRAKI.hpp
@@ -0,0 +1,14 @@
+#ifndef JKTROUGAO_HPP_INCLUDED
+#define JKTROUGAO_HPP_INCLUDED
+
+#include "Trougao.hpp"
+
+class JKTrougao : public Trougao
+{
+    public:
+        JKTrougao() : Trougao(3, 5, 5) {}
+        JKTrougao(double aa, double bb) : Trougao(aa, bb, bb) {}
+        JKTrougao(const JKTrougao &jt) : Trougao(jt.a, jt.b, jt.c) {}
+};
+
+#endif // JKTROUGAO_HPP_INCLUDED
