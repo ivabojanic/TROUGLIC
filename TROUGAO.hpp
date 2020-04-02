1459c385f80848f0996813fa00cabadfd1dfc87e
ivabojanic <62704225+ivabojanic@users.noreply.github.com>
2020-04-02 21:02:17 +0200

    Create TROUGAO.hpp

---
+++ TROUGAO.hpp
@@ -0,0 +1,31 @@
+#ifndef TROUGAO_HPP_INCLUDED
+#define TROUGAO_HPP_INCLUDED
+
+#include <cmath>
+
+class Trougao
+{
+    protected:
+        double a, b, c;
+    public:
+        Trougao() { a = 3; b = 4; c = 5; }
+        Trougao(double aa, double bb, double cc) { a = aa; b = bb; c = cc; }
+        Trougao(const Trougao &t) { a = t.a; b = t.b; c = t.c; }
+
+        void setA(double aa) { a = aa; }
+        void setB(double bb) { b = bb; }
+        void setC(double cc) { c = cc; }
+        
+        double getA() const { return a; }
+        double getB() const { return b; }
+        double getC() const { return c; }
+        double getO() const { return a + b + c; }
+
+        double getP() const
+        {
+            double P = (a + b + c) / 2;
+            return sqrt(P * (P - a) * (P - b) * (P - c));
+        }
+};
+
+#endif // TROUGAO_HPP_INCLUDED
